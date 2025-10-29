1f4177a92b1e189d42f67cbea71b0199cde1860c
mufidibm <nmufidibrahim02@gmail.com>
2025-10-29 10:29:27 +0700

    responsive

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: './',
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact/index.html'),
+      },
+    },
+  },
 })
